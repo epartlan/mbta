00ea4bff3db7be3ce1387879782c6f96be446383
Erin Partlan <52188112+epartlan@users.noreply.github.com>
2019-07-06 18:13:19 -0400

    Updated populatetraveltimes command
    If there already exists data for the last day, populatetraveltimes command now deletes the existing data for the last day and fetches updated data from MBTA to insert into the database.

--- polls/management/commands/populatetraveltimes.py
+++ polls/management/commands/populatetraveltimes.py
@@ -42,20 +42,18 @@ class Command(BaseCommand):
 				to_datetime = to_datetime + timedelta(days=1)
 				from_datetime_epoch = to_datetime_epoch				
 				to_datetime_epoch = str(int(mktime(to_datetime.timetuple())))
-		else:
-			tt = TravelTimes.objects.order_by('-from_datetime').values('from_datetime')[0]
-			from_datetime_epoch = tt['from_datetime']
+		else: 
+			from_datetime_epoch = str(TravelTimes.objects.order_by('-from_datetime').values_list('from_datetime', flat=True)[0])
 			from_datetime = datetime.fromtimestamp(int(from_datetime_epoch))
 
 			last_day = datetime.today() - timedelta(days=1)
 			last_day = datetime(last_day.year, last_day.month, last_day.day)
 			last_day_epoch = str(int(mktime(last_day.timetuple())))
-			
-			while from_datetime_epoch < last_day_epoch:
-				from_datetime = from_datetime + timedelta(days=1)
+
+			if from_datetime_epoch == last_day_epoch:
 				to_datetime = from_datetime + timedelta(days=1)
-				from_datetime_epoch = str(int(mktime(from_datetime.timetuple())))
 				to_datetime_epoch = str(int(mktime(to_datetime.timetuple())))
+				tt = TravelTimes.objects.order_by('-from_datetime').values('from_datetime')[0]
 				for j in range(2):
 					url = ('http://realtime.mbta.com/developer/api/v2.1/traveltimes?' +
 					'api_key=wX9NwuHnZU2ToO7GmGR9uw&format=json&from_stop=' +
@@ -64,7 +62,25 @@ class Command(BaseCommand):
 					'&to_datetime=' + to_datetime_epoch)
 					r = requests.get(url)
 					R = r.json()
-					print("Adding data for date: " + str(from_datetime))
-					tt = TravelTimes.objects.create(from_datetime=from_datetime_epoch, direction=j)
+					print("Updating data for date: " + str(from_datetime))
+					DepartureDates.objects.filter(travel_time=tt).delete()
 					for x in range(len(R['travel_times'])):
 						dd = DepartureDates.objects.create(departure_date=R['travel_times'][x]['dep_dt'], travel_time=tt)
+			else:			
+				while from_datetime_epoch < last_day_epoch:
+					from_datetime = from_datetime + timedelta(days=1)
+					to_datetime = from_datetime + timedelta(days=1)
+					from_datetime_epoch = str(int(mktime(from_datetime.timetuple())))
+					to_datetime_epoch = str(int(mktime(to_datetime.timetuple())))
+					for j in range(2):
+						url = ('http://realtime.mbta.com/developer/api/v2.1/traveltimes?' +
+						'api_key=wX9NwuHnZU2ToO7GmGR9uw&format=json&from_stop=' +
+						from_stops[j] + '&to_stop=' + to_stops[j] +
+						'&from_datetime=' + from_datetime_epoch +
+						'&to_datetime=' + to_datetime_epoch)
+						r = requests.get(url)
+						R = r.json()
+						print("Adding data for date: " + str(from_datetime))
+						tt = TravelTimes.objects.create(from_datetime=from_datetime_epoch, direction=j)
+						for x in range(len(R['travel_times'])):
+							dd = DepartureDates.objects.create(departure_date=R['travel_times'][x]['dep_dt'], travel_time=tt)
